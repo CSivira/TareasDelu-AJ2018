cae53ace6363c5a2f4f4affa662bc6d4d532ea0b
KevinMena <kevin_mena3@hotmail.com>
2018-05-28 22:32:56 -0400

    Tarea 1 Kevin Mena

---
+++ Programación/Actividad1/Actividad 1 Kevin Mena/TareasDELU/Assets/Scripts/Mov_Cajita2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov_Cajita2D : MonoBehaviour {
+
+	[SerializeField] float maxSpeed = 15f;
+	float velocidadActual = 0f;
+	Rigidbody rb;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		velocidadActual = Input.GetAxis("Horizontal") * maxSpeed;
+	}
+
+	void FixedUpdate () {
+		Move();
+	}
+
+	void Move() {
+		rb.velocity = new Vector3(velocidadActual, rb.velocity.y, rb.velocity.z);
+	}	
+}
---
+++ Programación/Actividad1/Actividad 1 Kevin Mena/TareasDELU/Assets/Scripts/Mov_Cajita3D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov_Cajita3D : MonoBehaviour {
+
+	[SerializeField] float maxSpeed = 15f;
+	Vector2 velocidadActual = new Vector2();
+	Rigidbody rb;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		velocidadActual.x = Input.GetAxis("Horizontal") * maxSpeed;
+		velocidadActual.y = Input.GetAxis("Vertical") * maxSpeed;
+	}
+
+	void FixedUpdate () {
+		Move();
+	}
+
+	void Move() {
+		rb.velocity = new Vector3(velocidadActual.x, rb.velocity.y, velocidadActual.y);
+	}	
+}
